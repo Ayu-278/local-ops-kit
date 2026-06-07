d077b21e7f16ff27fb59ee0ce25f5f2f2cdcafc6
Ayu-278 <18795000203@163.com>
2026-06-07 11:44:40 +0800

    Track storefront checkout sources

--- store-config.js
+++ store-config.js
@@ -1,7 +1,7 @@
 window.LOCAL_OPS_STORE = {
-  launchCheckoutUrl: "https://localopskit.gumroad.com/l/local-ops-n8n-kit",
-  standardCheckoutUrl: "https://localopskit.gumroad.com/l/local-ops-n8n-kit",
-  agencyCheckoutUrl: "https://localopskit.gumroad.com/l/local-ops-n8n-kit",
+  launchCheckoutUrl: "https://localopskit.gumroad.com/l/local-ops-n8n-kit?utm_source=github_pages&utm_medium=storefront&utm_campaign=local_ops_launch&utm_content=launch",
+  standardCheckoutUrl: "https://localopskit.gumroad.com/l/local-ops-n8n-kit?utm_source=github_pages&utm_medium=storefront&utm_campaign=local_ops_launch&utm_content=standard",
+  agencyCheckoutUrl: "https://localopskit.gumroad.com/l/local-ops-n8n-kit?utm_source=github_pages&utm_medium=storefront&utm_campaign=local_ops_launch&utm_content=agency",
   analyticsDomain: "",
   supportEmail: ""
 };
